e101e82c70fd83d4b73c5d78a939ec22b9502808
Mansing <mshinde@mediaocean.com>
2021-04-18 19:56:17 +0530

    tested javascript.

--- src/main/resources/js/my-plugin.js
+++ src/main/resources/js/my-plugin.js
@@ -1,37 +1,14 @@
-(() => {
-    // history.pushSate does not trigger any event so we need to hack it
-    // https://stackoverflow.com/questions/6390341/how-to-detect-if-url-has-changed-after-hash-in-javascript
-    history.pushState = ( f => function pushState(){
-        const ret = f.apply(this, arguments);
-        dispatchEvent(new PopStateEvent('popstate', null))
-        return ret;
-    })(history.pushState);
+console.log("Start of javascript");
 
-    const getCurrentBranchRef = () => {
-        const branch = new URLSearchParams(document.location.search).get('at');
+AJS.$("#dialog-show-button").click(function(e) {
+    e.preventDefault();
+    AJS.dialog2("#demo-dialog").show();
+});
 
-        return branch || 'refs/heads/master'; // Fallback to default branch ref (it can be any other branch than master)
-    }
+// Hides the dialog
+AJS.$("#dialog-submit-button").click(function (e) {
+    e.preventDefault();
+    AJS.dialog2("#demo-dialog").hide();
+});
 
-    const updateBranchLabel  = () => {
-        const baseBranch = getCurrentBranchRef();
-
-        container.innerHTML = `Current branch ref is: <strong>${baseBranch}</strong>`;
-    };
-
-    let container;
-
-    document.addEventListener('readystatechange', () => {
-        if (document.readyState !== 'complete') {
-            return;
-        }
-
-        container = document.querySelector('#my-plugin');
-
-        updateBranchLabel();
-    });
-
-    window.addEventListener('popstate', () => {
-        updateBranchLabel();
-    });
-})();
+console.log("End of javascript")
